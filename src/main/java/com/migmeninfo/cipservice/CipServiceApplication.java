931745e13f4eb05c98ecae6e3f22df45c3045a90
XCheise <kelvin@migmeninfo.com>
2022-09-22 20:42:38 +0000

    bootstrap application

---
+++ src/main/java/com/migmeninfo/cipservice/CipServiceApplication.java
@@ -0,0 +1,13 @@
+package com.migmeninfo.cipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CipServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/migmeninfo/cipservice/CipServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migmeninfo.cipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
